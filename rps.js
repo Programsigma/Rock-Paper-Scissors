f2d8780338e28924c9f90d60a35e027bd866c075
Teed <26TeedJ@badinhs.org>
2025-02-28 13:43:04 -0500

    made some skeleton, can't get prompt to work

--- rps.js
+++ rps.js
@@ -1,1 +1,20 @@
 // this is rock paper scissors
+/* prompt rock paper scissors
+assign rock, paper, and scissors to numbers 1 2 3
+generate a random number between 1 and 3
+if the respective numbers win, output a victory message
+*/
+
+let answer = undefined;
+function gooner(){
+     answer = prompt("Please input rock, paper, or scissors")
+}
+gooner()
+let rock = 1
+let paper = 2
+let scissors = 3
+let botAnswer = Math.random(1,3)
+for(let rock = 1; rock == answer; rock++) {
+    if(answer == botAnswer, console.log("Tie!"))
+}
+console.log(answer)
