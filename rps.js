0f44e3ab6fe3e668ec2ff2a2e8aa032f01923ac7
Teed <26TeedJ@badinhs.org>
2025-03-06 13:38:25 -0500

    Added score board, made it so only valid answers can be input

--- rps.js
+++ rps.js
@@ -6,10 +6,16 @@ if the respective numbers win, output a victory message
 */
 let botAnswer = undefined;
 let answer = undefined;
-
+let botScore = 0
+let playerScore = 0
 function RPS(){
     botAnswer = Math.ceil(Math.random() * 3)
     answer = prompt("Please input rock, paper, or scissors");
+    answer = answer.toLowerCase()
+    if(answer != "rock" && answer != "paper" && answer !="scissors") {
+            console.log("Not a valid input, try again.")
+            RPS();
+        }
     // IF answer == "rock" -> answer = 1
         if (answer == "rock"){ 
             answer = 1
@@ -18,9 +24,12 @@ function RPS(){
             } 
                 if(botAnswer == (answer + 2)){
                     console.log("Scissors you win!")
+                    playerScore = playerScore + 1
                 }
                 if(botAnswer == answer + 1){
-                    console.log("Paper you lose :(")}
+                    console.log("Paper you lose :(")
+                    botScore = botScore + 1
+                }
         }
         
         if (answer == "paper"){
@@ -30,9 +39,11 @@ function RPS(){
                 }
                 if(botAnswer == answer - 1){
                     console.log("Rock you win!")
+                    playerScore = playerScore + 1
                 }
                 if(botAnswer == answer + 1){
                     console.log("Scissors you lose :(")
+                    botScore = botScore + 1
                 }
         }
 
@@ -43,10 +54,28 @@ function RPS(){
                 }
                 if(botAnswer == answer - 1){
                     console.log("Paper you win!")
+                    playerScore = playerScore + 1
                 }
                 if(botAnswer == answer - 2){
                     console.log("Rock you lose :(")
+                    botScore = botScore + 1
                 }
         }
+      console.log(` Bots score is ${botScore}`)  
+      console.log(`Your score is ${playerScore}`)
+      if(botScore + playerScore == 5) {
+        if(playerScore<botScore){
+            console.log("You lose the game :(")
+            botScore = 0
+            playerScore = 0
+        }
+        if(playerScore>botScore){
+            console.log("You win the game :)")
+            botScore = 0
+            playerScore = 0
+        }
+        
      }
-RPS();
+    }
+
+ RPS();
